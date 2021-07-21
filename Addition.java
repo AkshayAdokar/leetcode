c3479e700d8a813048775693ed5194cbc9367650
Akshay Adokar <akshay.adokar@gmail.com>
2021-07-21 12:37:54 +0530

    Addition program

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println(addNos(100, 20));
+	}
+	
+	public static int addNos(int a , int b) {
+		return a + b;
+	}
+}
